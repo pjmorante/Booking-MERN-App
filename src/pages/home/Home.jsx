6bff51a430418c02f860d0c5284196093fafdc4e
Pedro Morante <pedro.morante@outlook.com>
2022-06-17 20:51:39 -0500

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from '../../components/navbar/NavBar';
+import Header from '../../components/header/Header';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import NavBar from '../../components/navbar/NavBar'
+
+const List = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header type="list"/>
+    </div>
+  )
+}
+
+export default List
