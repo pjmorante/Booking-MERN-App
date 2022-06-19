caa49b5ccbbe9c32e67c0cdbbc0888c4f84292cd
Pedro Morante <pedro.morante@outlook.com>
2022-06-18 19:08:35 -0500

    add footer and propertiesList components

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import NavBar from '../../components/navbar/NavBar'
+import './list.css'
 
 const List = () => {
   return (
