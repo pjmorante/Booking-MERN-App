96d0b2c64738e07830e08fd4185e56882300f4ed
Pedro Morante <pedro.morante@outlook.com>
2022-06-18 22:27:48 -0500

    create client folder

---
+++ client/src/pages/hotel/Hotel.jsx
@@ -0,0 +1,128 @@
+import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+
+import NavBar from '../../components/navbar/NavBar';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import MailList from '../../components/mailList/MailList';
+
+import './hotel.css';
+
+const Hotel = () => {
+
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const photos = [
+    {
+      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
+    },
+    {
+      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg',
+    },
+    {
+      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg',
+    },
+    {
+      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
+    },
+    {
+      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
+    },
+    {
+      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg',
+    },
+  ];
+
+  const handleOpen = (i) => {
+    setSlideNumber(i);
+    setOpen(true);
+  }
+
+  const handleMove = (direction) => {
+    let newSlideNumber;
+
+    if(direction === "l"){
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1
+    }else {
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+    }
+    setSlideNumber(newSlideNumber);
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        {open && (
+          <div className="slider">
+            <FontAwesomeIcon icon={faCircleXmark} className="close" onClick={() => setOpen(false)}/>
+            <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={() => handleMove("l")}/>
+            <div className="sliderWrapper">
+              <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+            </div>
+            <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={() => handleMove("r")}/>
+          </div>
+        )}
+        <div className="hotelWrapper">
+          <button className="bookNow">Reserve or Book Now!</button>
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St. 123 Somewhere</span>
+          </div>
+          <span className="hotelDistance">
+            Excellent location - 500m from Downtown
+          </span>
+          <span className="hotelPriceHighlight">
+            Book a stay over $114 at this property and get a free airport taxi
+          </span>
+          <div className="hotelImages">
+            {photos.map((photo, i) => (
+              <div className="hotelImgWrapper">
+                <img
+                  onClick={() => handleOpen(i)}
+                  src={photo.src}
+                  alt=""
+                  className="hotelImg"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of somewhere!</h1>
+              <p className="hotelDesc">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Reiciendis nemo error deleniti sapiente? Accusamus voluptatem
+                pariatur voluptas deserunt iste quaerat, laboriosam dolorem
+                eveniet. Est maxime culpa modi quas, optio ad!lorem Lorem ipsum,
+                dolor sit amet consectetur adipisicing elit. Reiciendis nemo
+                error deleniti sapiente? Accusamus voluptatem pariatur voluptas
+                deserunt iste quaerat, laboriosam dolorem eveniet. Est maxime
+                culpa modi quas, optio ad!lorem
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>
+                Located in the heart of somewhere, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+        <MailList />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Hotel;
