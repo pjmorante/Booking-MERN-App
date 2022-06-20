3b8302b75a3a6e33954cc646b1e5b37a0dc480de
Pedro Morante <pedro.morante@outlook.com>
2022-06-20 17:57:59 -0500

    add authentication and authorization

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,12 +1,26 @@
 import express from 'express';
+
 import {
   deleteUser,
   updateUser,
   getUser,
   getAllUsers,
 } from '../controllers/user.controller.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../ultils/verifyToken.js';
 
 const router = express.Router();
+
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//   res.send('You are logged in');
+// });
+
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//   res.send('You are logged in and you can delete your account');
+// });
+
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//   res.send('Hello Admin, You are logged in');
+// });
 
 router.put('/:id', updateUser);
 
