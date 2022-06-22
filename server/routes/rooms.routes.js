eea667eb694c482a915f4cd879be11913374e30f
Pedro Morante <pedro.morante@outlook.com>
2022-06-21 21:53:41 -0500

    add Room model anf Room routes

--- server/routes/rooms.routes.js
+++ server/routes/rooms.routes.js
@@ -1,5 +1,18 @@
 import express from 'express';
 
+import { createRoom, deleteRoom, getAllRooms, getRoom, updateRoom } from '../controllers/room.controller.js';
+import { verifyAdmin } from '../ultils/verifyToken.js';
+
 const router = express.Router();
+
+router.post('/:hotelId', verifyAdmin, createRoom);
+
+router.put('/:id', verifyAdmin, updateRoom);
+
+router.delete('/:id/:hotelId', verifyAdmin, deleteRoom);
+
+router.get('/:id', getRoom);
+
+router.get('/', getAllRooms);
 
 export default router;
