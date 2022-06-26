cb5ea4a8abd07ef77b9da5dc7390af5c161beca3
Pedro Morante <pedro.morante@outlook.com>
2022-06-26 15:22:06 -0500

    update Hotel component

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,39 +1,45 @@
 import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import NavBar from '../../components/navbar/NavBar';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import MailList from '../../components/mailList/MailList';
-
+import useFetch from '../../hooks/useFetch';
 import './hotel.css';
 
 const Hotel = () => {
 
+  const location = useLocation();
+  const id = location.pathname.split('/')[2];
+
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
 
-  const photos = [
-    {
-      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
-    },
-    {
-      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg',
-    },
-    {
-      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg',
-    },
-    {
-      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
-    },
-    {
-      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
-    },
-    {
-      src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg',
-    },
-  ];
+  const { data, loading, error } = useFetch(`/hotels/find/${id}`);
+
+  // const photos = [
+  //   {
+  //     src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
+  //   },
+  //   {
+  //     src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg',
+  //   },
+  //   {
+  //     src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg',
+  //   },
+  //   {
+  //     src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
+  //   },
+  //   {
+  //     src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg',
+  //   },
+  //   {
+  //     src: 'https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg',
+  //   },
+  // ];
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -55,36 +61,36 @@ const Hotel = () => {
     <div>
       <NavBar />
       <Header type="list" />
-      <div className="hotelContainer">
+      {loading ? 'loading' : (<div className="hotelContainer">
         {open && (
           <div className="slider">
             <FontAwesomeIcon icon={faCircleXmark} className="close" onClick={() => setOpen(false)}/>
             <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={() => handleMove("l")}/>
             <div className="sliderWrapper">
-              <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+              <img src={data.photos[slideNumber]} alt="" className="sliderImg" />
             </div>
             <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={() => handleMove("r")}/>
           </div>
         )}
         <div className="hotelWrapper">
           <button className="bookNow">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Grand Hotel</h1>
+          <h1 className="hotelTitle">{data.name}</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot} />
-            <span>Elton St. 123 Somewhere</span>
+            <span>{data.address}</span>
           </div>
           <span className="hotelDistance">
-            Excellent location - 500m from Downtown
+            Excellent location - {data.distance}m from Downtown
           </span>
           <span className="hotelPriceHighlight">
-            Book a stay over $114 at this property and get a free airport taxi
+            Book a stay over ${data.cheapestPrice} at this property and get a free airport taxi
           </span>
           <div className="hotelImages">
-            {photos.map((photo, i) => (
+            {data.photos?.map((photo, i) => (
               <div className="hotelImgWrapper">
                 <img
                   onClick={() => handleOpen(i)}
-                  src={photo.src}
+                  src={photo}
                   alt=""
                   className="hotelImg"
                 />
@@ -93,16 +99,9 @@ const Hotel = () => {
           </div>
           <div className="hotelDetails">
             <div className="hotelDetailsTexts">
-              <h1 className="hotelTitle">Stay in the heart of somewhere!</h1>
+              <h1 className="hotelTitle">{data.title}</h1>
               <p className="hotelDesc">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Reiciendis nemo error deleniti sapiente? Accusamus voluptatem
-                pariatur voluptas deserunt iste quaerat, laboriosam dolorem
-                eveniet. Est maxime culpa modi quas, optio ad!lorem Lorem ipsum,
-                dolor sit amet consectetur adipisicing elit. Reiciendis nemo
-                error deleniti sapiente? Accusamus voluptatem pariatur voluptas
-                deserunt iste quaerat, laboriosam dolorem eveniet. Est maxime
-                culpa modi quas, optio ad!lorem
+                {data.desc}
               </p>
             </div>
             <div className="hotelDetailsPrice">
@@ -120,7 +119,7 @@ const Hotel = () => {
         </div>
         <MailList />
         <Footer />
-      </div>
+      </div>)}
     </div>
   );
 };
